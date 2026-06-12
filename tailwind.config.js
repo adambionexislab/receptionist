b821ca810d57f79323f3de90b5ca260dbf54ef7e
Adam Gombala <gombalaadam650@gmial.com>
2026-06-12 15:30:03 +0200

    seo optimatization

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./backend/landingpage/**/*.html'],
+  theme: {
+    extend: {
+      fontFamily: { sans: ['Outfit', 'sans-serif'] },
+    },
+  },
+  plugins: [],
+};
